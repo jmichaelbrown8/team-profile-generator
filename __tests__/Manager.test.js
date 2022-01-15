d46fd6499674852627476271160bfc3f7fc1a3ee
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-01-15 14:24:50 -0800

    Finished off the tests and the classes with the Manager class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,18 +1,34 @@
-const Intern = require('../lib/Manager');
-throw new Error('Unimplimented');
+const Manager = require('../lib/Manager');
+
 describe('Manager', () => {
     describe('constructor', () => {
-        it('should work with expected input', () => {})
+        it('should work when passing in the required fields', () => {
+            const myEmployee = new Manager(20, 'John Doe', 'johndoe@generic.com', 5);
 
-        it('should throw an error with unexpected input', () => {})
+            // it should still work with the super class
+            expect(myEmployee.id).toEqual(20);
+            expect(myEmployee.name).toEqual('John Doe');
+            expect(myEmployee.email).toEqual('johndoe@generic.com');
 
-        it('should have the properties of the Employee class', () => {})
-    
+            // and collect the school property
+            expect(myEmployee.officeNumber).toEqual(5);
+
+        })
+
+        it('should throw an error when no officeNumber is passed', () => {
+            
+            const cb = () => {
+                const myEmployee = new Manager(20, 'John Doe', 'johndoe@generic.com');
+            }
+
+            expect(cb).toThrow(new Error("You must include officeNumber for a Manager class"));
+        })
     })
 
     describe('getRole', () => {
         it('should return overwrite the base Employee class and return "Manager"', () => {
-
+            const myEmployee = new Manager(20, 'John Doe', 'johndoe@generic.com', 5);
+            expect(myEmployee.getRole()).toEqual('Manager');
         })
     })
 
